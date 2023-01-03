132c5a85a086fe9edc3fb4bd8b1d5b5689c78ad7
k_126 <k_126@naver.com>
2023-01-03 14:03:27 +0900

    finish

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_list.html', {'post':post})
+    return render(request, 'blog/post_detail.html', {'post':post})
 
 def post_new(request):
     if request.method == "POST":
@@ -30,13 +30,13 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(instance=post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
+    else:
             form = PostForm(instance=post)
-        return render(request, 'blog/post_edit.html', {'form':form})
+    return render(request, 'blog/post_edit.html', {'form':form})
